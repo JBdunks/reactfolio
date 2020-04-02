7c034fb363c42e02928a53aab5e4cb685d7b46b5
Jon Butler <Jwb2472@gmail.com>
2020-04-01 23:46:55 -0400

    set up file structure

---
+++ src/components/main/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Project from "../project/project";
+const Main = () => {
+  return (
+    <>
+      <div>i contain the project cards</div>
+
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </>
+  );
+};
+
+export default Main;
