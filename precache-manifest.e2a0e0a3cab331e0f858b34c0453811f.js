0673bfcc96bf7548d08b7ef9a016d01e73cdf7ed
Jon Butler <Jwb2472@gmail.com>
2020-04-09 16:06:02 -0400

    Updates

--- precache-manifest.e2a0e0a3cab331e0f858b34c0453811f.js
+++ precache-manifest.e2a0e0a3cab331e0f858b34c0453811f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dca6f9f1ff651b8fc35f881861efa012",
+    "revision": "f0a68059d736e02150898dcc896a5451",
     "url": "/reactfolio/index.html"
   },
   {
-    "revision": "8f76dc8a116b2840db13",
+    "revision": "280c1acfa378a82e0267",
     "url": "/reactfolio/static/css/main.e2bcc682.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactfolio/static/js/2.b677396a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8f76dc8a116b2840db13",
-    "url": "/reactfolio/static/js/main.b92e0011.chunk.js"
+    "revision": "280c1acfa378a82e0267",
+    "url": "/reactfolio/static/js/main.ee01f2c7.chunk.js"
   },
   {
     "revision": "6d09cb3d02fd0f7a7de1",
